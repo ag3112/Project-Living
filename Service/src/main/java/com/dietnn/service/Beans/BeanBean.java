5a8262b0942f7adde1afcaddfa6a4faaa7def858
Ankit <ag_3112@outlook.com>
2015-11-08 23:28:04 +0530

    Added BEans

---
+++ Service/src/main/java/com/dietnn/service/Beans/BeanBean.java
@@ -0,0 +1,7 @@
+package com.dietnn.service.beans;
+
+/**
+ * Created by Intel on 11/8/2015.
+ */
+public interface BeanBean {
+}
---
+++ Service/src/main/java/com/dietnn/service/Beans/User.java
@@ -0,0 +1,7 @@
+package com.dietnn.service.Beans;
+
+/**
+ * Created by Intel on 11/2/2015.
+ */
+public class User {
+}
